67b60b46328ea49f216f12e7f586f297092f9b0b
Igor <igortadic.msc@gmail.com>
2024-10-13 13:47:47 +0200

    02 - passed all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, repeatTimes) {
+    let newString = '';
 
+    if (repeatTimes < 0) {
+        return 'ERROR';
+    }
+
+    for (let i = 0; i < repeatTimes; i++) {
+        newString += string;
+    }
+    return newString;
 };
 
 // Do not edit below this line
