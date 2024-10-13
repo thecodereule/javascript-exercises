06ae16de5593a0c26559facceaf3b20ec062acc5
Igor <igortadic.msc@gmail.com>
2024-10-13 22:25:51 +0200

    05 - passed tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(...theArgs) {
 
+    if (theArgs.some((num) => num < 0 || typeof num !== 'number' || !Number.isInteger(num))) {
+        return 'ERROR';
+    }
+
+    const max = Math.max(...theArgs);
+    const min = Math.min(...theArgs);
+    
+    let sum = 0;
+
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
