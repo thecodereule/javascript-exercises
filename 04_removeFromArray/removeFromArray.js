5264f80734f09f96d180457d80a62f659258e2e7
Igor <igortadic.msc@gmail.com>
2024-10-13 21:37:11 +0200

    04 - passed tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...theArgs) {
+
+    return array.filter(element => !theArgs.includes(element));
+
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
