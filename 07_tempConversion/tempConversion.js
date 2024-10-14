a1186afbb1d883e0a7151fd6d45a04b480e968e7
Igor <igortadic.msc@gmail.com>
2024-10-14 04:21:28 +0200

    07 - Passed tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
 const convertToCelsius = function() {
+  const celsius = (arguments[0] - 32) * 5/9;
+  
+  return (Math.round(celsius * 10) / 10);
 };
 
 const convertToFahrenheit = function() {
+  const fahrenheit = arguments[0] * 9/5 + 32; 
+  return (Math.round(fahrenheit * 10) / 10);
 };
 
 // Do not edit below this line
