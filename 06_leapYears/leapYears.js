e0faabb6cced1615923b780b475cf7ecbc4615eb
Igor <igortadic.msc@gmail.com>
2024-10-14 04:05:18 +0200

    06 - Passed tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
 const leapYears = function() {
-
-};
+    if (arguments[0] % 400 === 0 && arguments[0] % 100 === 0) {
+        return true;
+    } else if (arguments[0] % 100 === 0) {
+        return false;
+    } else if (arguments[0] % 4 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 // Do not edit below this line
 module.exports = leapYears;
